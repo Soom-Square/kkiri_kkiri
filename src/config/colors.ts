eb25de05733243b3f5eebebbdb0bb04914c06175
Beomsoo <linoleca@naver.com>
2025-10-21 20:41:43 +0900

    로그인화면 UI 수정

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -3,6 +3,8 @@ const colors = {
   inputBackground: '#F2F4F7',
   inputPlaceholder: '#667085',
   inputText: '#101828',
+  inputBorder: '#E4E7EC',
+  background: '#F9FAFB',
 };
 
 export default colors;
